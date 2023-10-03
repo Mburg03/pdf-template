d70d34ccc56dab26db9845506da2c5d099dfcea4
Mario Umaña <mburgosgit003@gmail.com>
2023-10-02 23:38:18 -0600

    Add: writelines and page personalization

--- main.py
+++ main.py
@@ -6,20 +6,30 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("./topics.csv")
 pdf_page = 0
+
 for index, row in df.iterrows():    
     for i in range(row["Pages"]):
+        line_height = 22        
         pdf_page += 1
+        
+        # Creating pages
         pdf.add_page()
+        pdf.set_draw_color(0,0,0)
         pdf.set_font(family="Times", style="B", size=24)
-        pdf.set_text_color(100,100,100)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
         pdf.line(10, 22, 200, 22)
         
-        # Setting the footer
-        pdf.ln(255)
+        # Creating writelines
+        for i in range(25):
+            line_height += 10
+            pdf.set_draw_color(180,180,180)
+            pdf.line(15, line_height, 195, line_height)
+
+        # Setting the footer of the pdf file
+        pdf.ln(260)
         pdf.set_text_color(180,180,180)
         pdf.set_font(family="Times", style="B", size=12)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="L")
         pdf.cell(w=0, h=10, txt=f"Page {str(pdf_page)}", align="R")
 
-pdf.output("output.pdf")
+pdf.output("notebook.pdf")
